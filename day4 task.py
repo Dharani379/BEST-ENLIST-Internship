6afe1812e47bfa25c61d9077cf27c91041741762
Dharani379 <85785688+Dharani379@users.noreply.github.com>
2021-06-15 14:52:33 +0530

    Add files via upload

---
+++ day4 task.py
@@ -0,0 +1,56 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=50
+>>> print(a/10)
+5.0
+>>> print(b*50)
+2500
+>>> print(c+60)
+110
+>>> X="Bal is full of books"
+>>> print(X)
+Bal is full of books
+>>> print(X.replace("l","G"))
+BaG is fuGG of books
+>>> X="Bad is full of books"
+>>> print(X)
+Bad is full of books
+>>> print(X.replace("d","G"))
+BaG is full of books
+>>> #conversion of integers to float
+>>> #declaring int
+>>> y=12
+>>> print(Y)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    print(Y)
+NameError: name 'Y' is not defined
+>>> print(y)
+12
+>>> print(type(y))
+<class 'int'>
+>>> print("y is",type(y))
+y is <class 'int'>
+>>> y=float(y)
+>>> print(y)
+12.0
+>>> print(type(y))
+<class 'float'>
+>>> #converting float to int data type
+>>> #declaring float
+>>> z = 234.79
+>>> print(z)
+234.79
+>>> print(type(Z))
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    print(type(Z))
+NameError: name 'Z' is not defined
+>>> print(type(z))
+<class 'float'>
+>>> Z=int(z)
+>>> print(Z)
+234
+>>> print(type(Z))
+<class 'int'>
+>>> 
