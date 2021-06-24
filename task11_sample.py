855f04c4c0fd9d2fd5607521b4ee2ab5c9fb5746
Dharani379 <85785688+Dharani379@users.noreply.github.com>
2021-06-24 16:14:16 +0530

    Add files via upload

---
+++ task11_sample.py
@@ -0,0 +1,22 @@
+#creating a python module with list and import the module in another .py file and change the value in list
+pip install Module
+import mathprint("the value of pi is",math.pi)
+pip install pandas
+import pandas
+#import amodule that picks number and write a programe to fetch a random number from 1 to 100 on every run
+import numpy as np
+import random
+print(random.randint(0,9))
+
+print(random.randint(50,100))
+
+#import sys packages and find the python path
+
+import sys
+sys.path
+
+sys.version
+#Try to install a package and uninstall a package using pi
+pip install flask
+
+pip uninstall flask
