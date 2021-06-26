2e3a37e104f0494ea1a78dc369284680f775b0fa
Dharani379 <85785688+Dharani379@users.noreply.github.com>
2021-06-26 16:47:54 +0530

    Add files via upload

---
+++ task11_3.py
@@ -0,0 +1,10 @@
+from random import seed
+from random import randint
+seed(1)
+for i in range(100):
+    value = randint(1,100)
+    print(value)
+    
+import sys
+sys.path.append('C:/Users/mekad/AppData/Local/Programs/Python')
+print(sys.path)
