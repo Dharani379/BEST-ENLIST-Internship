125d479793b327546649b5128d3b87ba74890834
Dharani379 <85785688+Dharani379@users.noreply.github.com>
2021-07-15 16:16:08 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,7 @@
+#1
+list1=['dharani','sri','mahi','anitha','siva']
+print(list(enumerate(list1)))
+print(enumerate(list1))
+#2
+tuple1=('dharani','sri','mahi','anitha','siva')
+print(tuple(enumerate(tuple1)))
